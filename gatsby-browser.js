199f6c596da3d24901b15e1117b15822a283ddd8
LangitBiru6107 <langitbiru6107@gmail.com>
2020-12-18 12:13:40 +0700

    base styling

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,6 @@
-// custom typefaces
-import "typeface-montserrat";
-import "typeface-merriweather";
 // normalize CSS across browsers
-import "./src/normalize.css";
-// custom CSS styles
-import "./src/style.css";
+import "./src/styles/index.css";
+import "./src/styles/normalize.css";
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css";
