347ff1e5a3622c0a118e6c708b0007e0ab1aa1fd
LangitBiru6107 <langitbiru6107@gmail.com>
2020-12-23 09:43:53 +0700

    add caption image

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,8 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 630
+              maxWidth: 480,
+              showCaptions: true
             }
           },
           {
