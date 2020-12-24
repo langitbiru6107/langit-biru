0ae5f67c94d0459f30a39f47fe9133642f304d65
LangitBiru6107 <langitbiru6107@gmail.com>
2020-12-24 11:45:33 +0700

    extract post item list

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -2,6 +2,31 @@ import { Link } from "gatsby";
 import React from "react";
 import { PostDetailFragment } from "../../types/gatsby-graphql";
 
+const PostItemList: React.FC<{
+  slug?: string;
+  title?: string;
+  description?: string;
+  date?: string;
+  chapter?: number;
+}> = ({ slug, title, description, date, chapter }) => (
+  <article className="post-item__list">
+    <h2>
+      <Link to={slug}>
+        {chapter !== null && `Chapter ${chapter} : `}
+        {title}
+      </Link>
+    </h2>
+    {date && <time>{date}</time>}
+    <section>
+      <p
+        dangerouslySetInnerHTML={{
+          __html: description
+        }}
+      />
+    </section>
+  </article>
+);
+
 const PostItem: React.FC<{
   title: string;
   posts: PostDetailFragment["nodes"];
@@ -14,29 +39,9 @@ const PostItem: React.FC<{
         <p>No posts found.</p>
       ) : (
         <>
-          {posts.map(
-            ({
-              fields: { slug },
-              frontmatter: { title, description, date, chapter }
-            }) => (
-              <article key={slug} className="post-item__list">
-                <h2>
-                  <Link to={slug}>
-                    {chapter !== null && `Chapter ${chapter} : `}
-                    {title}
-                  </Link>
-                </h2>
-                {date && <time>{date}</time>}
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: description
-                    }}
-                  />
-                </section>
-              </article>
-            )
-          )}
+          {posts.map(({ fields, frontmatter }) => (
+            <PostItemList {...{ ...fields, ...frontmatter }} />
+          ))}
         </>
       )}
     </div>
